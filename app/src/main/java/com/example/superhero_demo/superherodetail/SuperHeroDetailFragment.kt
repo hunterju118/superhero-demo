0da178750e9ed176a3a0c2e94004dc2642a96468
HunterJ <hunterju118@gmail.com>
2023-03-16 19:14:56 +1100

    polish UI

--- app/src/main/java/com/example/superhero_demo/superherodetail/SuperHeroDetailFragment.kt
+++ app/src/main/java/com/example/superhero_demo/superherodetail/SuperHeroDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.superhero_demo.superherodetail
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,10 +10,9 @@ import com.example.superhero_demo.databinding.FragmentSuperHeroDetailBinding
 import com.example.superhero_demo.model.SuperHero
 import com.squareup.picasso.Picasso
 
-private const val SUPERHERO_KEY = ""
+private const val SUPERHERO_KEY = "SUPERHERO_KEY"
 
 class SuperHeroDetailFragment : Fragment() {
-
 
     private lateinit var binding: FragmentSuperHeroDetailBinding
 
@@ -29,14 +29,17 @@ class SuperHeroDetailFragment : Fragment() {
         setupView()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setupView() {
         val superHero = arguments?.getSerializable(SUPERHERO_KEY) as? SuperHero
         binding.apply outer@ {
             if (superHero != null) {
                 with(superHero) {
-                    ID.text = id
-                    this@outer.name.text = name
-                    fullName.text = biography.fullName
+                    ID.text = "ID: #$id"
+                    this@outer.name.text = "Title: $name"
+                    fullName.text = "Full name: ${biography.fullName}"
+                    gender.text = "Gender: ${appearance.gender}"
+                    race.text = "Race: ${appearance.race}"
                     intelligence.text = powerstats.intelligence
                     strength.text = powerstats.strength
                     speed.text = powerstats.speed
